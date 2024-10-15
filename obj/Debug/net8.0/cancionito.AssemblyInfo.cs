3d645a1d05dcc8804bd488228e9ed558368d98bc
Andrew <andrewblockernst@gmail.com>
2024-10-15 11:37:06 -0300

    Render deploy attempt 5

--- obj/Debug/net8.0/cancionito.AssemblyInfo.cs
+++ obj/Debug/net8.0/cancionito.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cancionito")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4e4afedb61ecd8d644008ffe704d99edacbd677")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+042819485c78e778f24bd89523dac34ee8a471e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cancionito")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cancionito")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
